a27cb587d173f1498ef4ed99f5bd1258d5e56ebf
LESSLOAD <60436902+lessload@users.noreply.github.com>
2020-03-01 12:50:54 +0000

    Update Reload-dnscrypt.bat

--- Reload-dnscrypt.bat
+++ Reload-dnscrypt.bat
@@ -9,14 +9,14 @@ ipconfig /flushdns
 
 :OfflineTest
 timeout /t 1 /nobreak
-powershell -Command "Invoke-WebRequest https://czd1.github.io/c.txt -OutFile '%Temp%\dnscrypt-check.txt' "
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/RH3GW47Q -OutFile '%Temp%\dnscrypt-check.txt' "
 for /F "delims=:" %%I in (%Temp%\dnscrypt-check.txt) do (if /I "czd" == "%%I" (echo x > "%Temp%\dnscrypt-check.txt" && GoTo OnlineRun) else (GoTo OfflineTest))
 
 :OnlineRun
 ::StartService
 dnscrypt-proxy.exe -service start
-::for temporary fix NCSI indicator in some case
-::timeout /t 15 /nobreak
+::temporary fix NCSI error
+timeout /t 5 /nobreak
 for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Connected"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
 ipconfig /flushdns
 ::pause
