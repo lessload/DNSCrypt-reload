02a73dc26efe3c407db4b9dfa7bb135b8d34251a
LESSLOAD <60436902+lessload@users.noreply.github.com>
2020-03-01 12:48:59 +0000

    Update lessload-installer.bat

--- lessload-installer.bat
+++ lessload-installer.bat
@@ -7,4 +7,9 @@ dnscrypt-proxy.exe -service uninstall
 dnscrypt-proxy.exe -service install
 for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Dedicated"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
 ipconfig /flushdns
+
+::NCSI-mod
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet" /v "PassivePollPeriod" /t REG_DWORD /d "5" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet" /v "EnableActiveProbing" /t REG_DWORD /d "0" /f
+
 pause
