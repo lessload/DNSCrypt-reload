009ce937e4e82057bee8eb843740caf56bfa6d47
LESSLOAD <60436902+lessload@users.noreply.github.com>
2020-06-02 11:44:25 +0700

    Update lessload-installer.bat
    - fix stop script from install loopback to VPN

--- lessload-installer.bat
+++ lessload-installer.bat
@@ -5,13 +5,11 @@ xcopy .\win64 "C:\Program Files\dnscrypt-proxy-win64\" /h/i/c/k/e/r/y
 cd /d "C:\Program Files\dnscrypt-proxy-win64\"
 dnscrypt-proxy.exe -service uninstall
 dnscrypt-proxy.exe -service install
-for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Dedicated"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
+for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Dedicated" ^| find /i /v "Local Area"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
 ipconfig /flushdns
 
-::NCSI-mod
+::NCSI modify
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet" /v "PassivePollPeriod" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet" /v "EnableActiveProbing" /t REG_DWORD /d "0" /f
-
-dnscrypt-proxy.exe -service start
 
 pause
