e507a2762cf3e2bd78aa6684e79ed09172650d51
LESSLOAD <60436902+lessload@users.noreply.github.com>
2020-07-07 01:53:45 -0700

    Update re-dnscrypt-v2.bat
    Performance improve in slow pc.

--- re-dnscrypt-v2.bat
+++ re-dnscrypt-v2.bat
@@ -1,4 +1,4 @@
-:: LESSLOAD-DNSCrypt v2.0 beta
+:: LESSLOAD-DNSCrypt v2.1 beta
 @echo off
 cd /d "C:\Program Files\dnscrypt-proxy-win64\"
 echo x > "%Temp%\dnscrypt-check.txt"
@@ -17,9 +17,7 @@ for /F "delims=:" %%I in (%Temp%\dnscrypt-check.txt) do (if /I "czd" == "%%I" (e
 :RunService
 ::StartService
 ::Don't forget edit script to your file location
-start "" powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/public-resolvers.md.minisig -OutFile 'C:\Program Files\dnscrypt-proxy-win64\public-resolvers.md.minisig'"
-start "" powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/relays.md -OutFile 'C:\Program Files\dnscrypt-proxy-win64\relays.md'"
-start "" powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/relays.md.minisig -OutFile 'C:\Program Files\dnscrypt-proxy-win64\relays.md.minisig'"
+start "" powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/public-resolvers.md.minisig -OutFile 'C:\Program Files\dnscrypt-proxy-win64\public-resolvers.md.minisig'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/relays.md -OutFile 'C:\Program Files\dnscrypt-proxy-win64\relays.md'; Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/relays.md.minisig -OutFile 'C:\Program Files\dnscrypt-proxy-win64\relays.md.minisig'"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v2/public-resolvers.md -OutFile 'C:\Program Files\dnscrypt-proxy-win64\public-resolvers.md'"
 dnscrypt-proxy.exe -service start
 for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Connected" ^| find /i /v "Local Area"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
